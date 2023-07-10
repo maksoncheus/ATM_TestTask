8e08e78b8737d16b3db73d11c94fb858dfa56378
Максим Макаров <maksoncheus@gmail.com>
2023-07-10 23:10:41 +0700

    Добавьте файлы проекта.

---
+++ ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM_TestTask
+{
+    internal class ViewModel
+    {
+        public ViewModel()
+        {
+            ATM atm = new ATM(1000);
+        }
+    }
+}
